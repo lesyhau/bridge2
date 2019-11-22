b5790ab758bf21651c210d5ff5ac953d91728b83
Hau Le <le.sy.hau@outlook.com>
2019-11-22 17:08:56 +0700

    Update TestCase.pm

--- TestCase.pm
+++ TestCase.pm
@@ -17,7 +17,7 @@ sub new
 		DesignItems			=> [],
 		ExpectedResults		=> [],
 		PreConditions		=> [],
-		PostCondtions		=> [],
+		PostConditions		=> [],
 		TestSteps			=> []
 	}, $class;
 
@@ -29,23 +29,26 @@ sub Name					{ $_[0]->{Name			} = $_[1] if defined $_[1]; $_[0]->{Name		}; }
 sub TestNumber				{ $_[0]->{TestNumber	} = $_[1] if defined $_[1]; $_[0]->{TestNumber	}; }
 sub TopTag					{ $_[0]->{TopTag		} = $_[1] if defined $_[1]; $_[0]->{TopTag		}; }
 sub BottomTag				{ $_[0]->{BottomTag		} = $_[1] if defined $_[1]; $_[0]->{BottomTag	}; }
+
 sub Goals					{ @{$_[0]->{Goals			}}; }
 sub DesignItems				{ @{$_[0]->{DesignItems		}}; }
 sub ExpectedResults			{ @{$_[0]->{ExpectedResults	}}; }
 sub PreConditions			{ @{$_[0]->{PreConditions	}}; }
-sub PostCondtions			{ @{$_[0]->{PostCondtions	}}; }
+sub PostConditions			{ @{$_[0]->{PostConditions	}}; }
 sub TestSteps				{ @{$_[0]->{TestSteps		}}; }
-sub AddGoal					{ @{$_[0]->{Goals			}}; }
+
+sub AddGoal					{ push @{$_[0]->{Goals			    }}, $_[1] if defined $_[1]; }
 sub AddDesignItem			{ push @{$_[0]->{DesignItems		}}, $_[1] if defined $_[1]; }
 sub AddExpectedResult		{ push @{$_[0]->{ExpectedResults	}}, $_[1] if defined $_[1]; }
 sub AddPreCondition			{ push @{$_[0]->{PreConditions		}}, $_[1] if defined $_[1]; }
-sub AddPostCondtion			{ push @{$_[0]->{PostCondtions		}}, $_[1] if defined $_[1]; }
+sub AddPostCondition		{ push @{$_[0]->{PostConditions	    }}, $_[1] if defined $_[1]; }
 sub AddTestStep				{ push @{$_[0]->{TestSteps			}}, $_[1] if defined $_[1]; }
+
 sub GoalsCount				{ scalar @{$_[0]->{Goals			}}; }
 sub DesignItemsCount		{ scalar @{$_[0]->{DesignItems		}}; }
 sub ExpectedResultsCount	{ scalar @{$_[0]->{ExpectedResults	}}; }
 sub PreConditionsCount		{ scalar @{$_[0]->{PreConditions	}}; }
-sub PostCondtionsCount		{ scalar @{$_[0]->{PostCondtions	}}; }
+sub PostCondtionsCount		{ scalar @{$_[0]->{PostConditions	}}; }
 sub TestStepsCount			{ scalar @{$_[0]->{TestSteps		}}; }
 
 sub PrintSpecs
@@ -59,37 +62,97 @@ sub PrintSpecs
 	my @designItems				= $_[0]->DesignItems			;
 	my @expectedResults			= $_[0]->ExpectedResults		;
 	my @preConditions			= $_[0]->PreConditions			;
-	my @postCondtions			= $_[0]->PostCondtions			;
+	my @postConditions			= $_[0]->PostConditions			;
 	my @testSteps				= $_[0]->TestSteps				;
-	my $goalsCount				= $_[0]->GoalsCount				;
-	my $designItemsCount		= $_[0]->DesignItemsCount		;
-	my $expectedResultsCount	= $_[0]->ExpectedResultsCount	;
-	my $preConditionsCount		= $_[0]->PreConditionsCount		;
-	my $postCondtionsCount		= $_[0]->PostCondtionsCount		;
-	my $testStepsCount			= $_[0]->TestStepsCount			;
+	# my $goalsCount				= $_[0]->GoalsCount				;
+	# my $designItemsCount		= $_[0]->DesignItemsCount		;
+	# my $expectedResultsCount	= $_[0]->ExpectedResultsCount	;
+	# my $preConditionsCount		= $_[0]->PreConditionsCount		;
+	# my $postCondtionsCount		= $_[0]->PostCondtionsCount		;
+	# my $testStepsCount			= $_[0]->TestStepsCount			;
 
 	my $i = 0;
 
 	print <<EOF;
+********************************************************************************
 $id Unit Test: $name
 $topTag
+Test Desciption
 EOF
 
 	$i = 0;
+    print <<EOF;
+Design Items
+EOF
 	foreach my $designItem (@designItems)
 	{
 		$i++;
 		print <<EOF;
-Design Item	$i	$designItem
+    $i	$designItem
 EOF
 	}
 
 	$i = 0;
+    print <<EOF;
+Goals
+EOF
 	foreach my $goal (@goals)
 	{
 		$i++;
 		print <<EOF;
-Goal	$i	$goal
+    $i	$goal
+EOF
+	}
+
+	$i = 0;
+    print <<EOF;
+Pre-Condition
+EOF
+	foreach my $preCondition (@preConditions)
+	{
+		$i++;
+		print <<EOF;
+    $i	$preCondition
+EOF
+	}
+
+	$i = 0;
+    print <<EOF;
+Test Steps
+EOF
+	foreach my $testStep (@testSteps)
+	{
+		$i++;
+		print <<EOF;
+    $i	$testStep
+EOF
+	}
+
+    print <<EOF;
+Test Case Number    $testNumber
+EOF
+
+	$i = 0;
+    print <<EOF;
+Expected Results
+EOF
+	foreach my $expectedResult (@expectedResults)
+	{
+		$i++;
+		print <<EOF;
+    $i	$expectedResult
+EOF
+	}
+
+	$i = 0;
+    print <<EOF;
+Post-Condition
+EOF
+	foreach my $postCondition (@postConditions)
+	{
+		$i++;
+		print <<EOF;
+    $i	$postCondition
 EOF
 	}
 
